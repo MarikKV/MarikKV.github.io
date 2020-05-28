da582f99cbe9fe4c57e5b16dd92938ca93f73ffd
Marik1 <kosaniakm@gmail.com>
2020-05-28 21:54:33 +0300

    fix js form

--- js/index.js
+++ js/index.js
@@ -12,7 +12,8 @@ function drawTable(){
       <td>${index + 1}</td>
       <td>${value.name}</td>
       <td>${value.task}</td>
-      <td><button value='${value.id}' onclick='myFunction(this.value)'>Del</button></td>
+      <td><button value='${value.id}' onclick='edit(this.value)'>Edit</button></td>
+      <td><button value='${value.id}' onclick='del(this.value)'>Delete</button></td>
     </tr>`
   })
   table.innerHTML = tableHtml;
@@ -28,16 +29,32 @@ db.collection('Users').onSnapshot(snapshot =>{
         el.id = change.doc.id
         data = [...data, el]
       }
+      //console.log('adding user')
       addData();
     } else if(change.type == 'removed'){
-      let addData = () => {
+      let remuveData = () => {
         const el = change.doc.data();
         el.id = change.doc.id
-        data = [...data, el]
+        data = data.filter(item => item.id !== el.id)
       }
-      addData();
+      //console.log('remuving user')
+      remuveData();
     } else {
-      console.log('another type')  
+      //console.log('updating user') 
+      
+      let updateData = () => {
+        const el = change.doc.data();
+        el.id = change.doc.id
+        let newIndex;
+        updated = data.map((item, index) => {
+          item.id == el.id ? newIndex = index : null
+        })
+        const name =  document.querySelector('#name');
+        const task =  document.querySelector('#task');
+        data[newIndex].name = name.value;
+        data[newIndex].task = task.value;
+      }
+      updateData()
     }
   })
   drawTable()
@@ -90,11 +107,39 @@ class AddingTask extends Users{
 
 
 //del task
-function myFunction(index){
-  console.log('index', index)
-  data = data.filter( val => val.id != index)
-  console.log('data', data)
-  drawTable()
+function del(index){
+  const user_to_delete = new Users();
+  user_to_delete.delUser(index);
+}
+
+function edit(index){
+  console.log('index of user - ', index)
+  const button = document.querySelector('#submitButton');
+  const name =  document.querySelector('#name');
+  const task =  document.querySelector('#task');
+
+  user = data.filter(item => item.id==index)
+  console.log(user)
+  name.value = user[0].name;
+  task.value = user[0].task;
+  button.value = 'Eddit task'
+  button.setAttribute('onclick',`edditTask('${index}')`);
+}
+function edditTask(index){
+  const name =  document.querySelector('#name');
+  const task =  document.querySelector('#task');
+  const button = document.querySelector('#submitButton');
+
+  const user_to_update = new Users(name.value, task.value);
+  user_to_update.updateUser(index)
+
+  function time(){
+    button.value = 'Add task'
+    button.setAttribute('onclick','submitForm()');
+    name.value = ''
+    task.value = ''
+  }
+  setTimeout(time, 100)
 }
 
 //add new task
---
+++ test.js
@@ -0,0 +1,14 @@
+// const a = [2, 3, 4, 5];
+
+// let result = a.reduce((a,e) => {
+//     console.log('a,e', a,e)
+//     if(e%2 == 0){
+//         return a+e
+//     }else{
+//         return a
+//     }
+// });
+
+// console.log(result);
+const a = [2, 3, 4, 5];
+console.log(a.indexOf(8) != -1);
