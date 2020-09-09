a6021019126c427f60042b58e140b8b528ae1c51
MarikKV <kosaniakm@gmail.com>
2020-09-09 22:02:48 +0300

    small fix

--- portfolio-site10/main.js
+++ portfolio-site10/main.js
@@ -7,9 +7,8 @@ const whatYouGetOption3 = document.querySelector('#what_you_get_3');
 
 function changeImage(block, image, e){
     block.innerHTML = `<img src="images/${image}" class="wow bounceInRight" alt="">`
-    console.log(e.target)
 }
 
-whatYouGetOption1.onmouseover = e => changeImage(whatYouGet, 'what_you_get1.svg', e)
-whatYouGetOption2.onmouseover = e => changeImage(whatYouGet, 'what_you_get2.svg', e)
-whatYouGetOption3.onmouseover = e => changeImage(whatYouGet, 'what_you_get3.svg', e)
+whatYouGetOption1.onmouseover = () => changeImage(whatYouGet, 'what_you_get1.svg')
+whatYouGetOption2.onmouseover = () => changeImage(whatYouGet, 'what_you_get2.svg')
+whatYouGetOption3.onmouseover = () => changeImage(whatYouGet, 'what_you_get3.svg')
