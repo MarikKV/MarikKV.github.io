7b084bf435da9de5fb0dcb83aeaea699be140915
DESKTOP-E5Q8V3G\Marian <marian@tarta.ai>
2026-02-17 19:03:15 +0200

    update styles and project info

--- projects.js
+++ projects.js
@@ -13,7 +13,8 @@ createApp({
                         "Support and develop new UI/UX features for web-app",
                         "Create multiple tools to work with pdf files"
                     ],
-                    video: "https://www.youtube.com/channel/UCa57I5DFqulQaoMR_0H--kA"
+                    video: "https://www.youtube.com/channel/UCa57I5DFqulQaoMR_0H--kA",
+                    website: "https://instafill.ai"
                 },
                 {
                     name: "Tarta.ai",
@@ -25,17 +26,19 @@ createApp({
                         "Worked on page load speed and SEO optimization of website",
                         "Created and supported admin tools to support job board, company profiles, articles, and other features"
                     ],
-                    video: "https://www.youtube.com/@tarta-ai"
+                    video: "https://www.youtube.com/@tarta-ai",
+                    website: "https://tarta.ai"
                 },
                 {
-                    name: "Bestaiapps.co",
-                    description: "Some description of site (support <b>html-markup</b>)",
+                    name: "Best AI Apps",
+                    description: "Market of plugins for ChatGPT. AI applications designed for personal and business use cases.",
                     image: "/project_images/tarta.png", // Or local path if available
                     tags: ["AI plugins", "AI-driven"],
                     responsibilities: [
 
                     ],
-                    video: "https://www.youtube.com/@tarta-ai" // Placeholder link
+                    video: "https://www.youtube.com/@bestaiapps-co",
+                    website: "https://bestaiapps.co"
                 },
                 // Add more projects here
             ]
