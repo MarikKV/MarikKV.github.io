66dda0295effe50b9ec50534be88859079d40510
DESKTOP-E5Q8V3G\Marian <marian@tarta.ai>
2026-02-24 14:31:27 +0200

    Add new project to list. Move images to new folder. Delete not used images

--- projects.ts
+++ projects.ts
@@ -13,22 +13,39 @@ export const projects: Project[] = [
     {
         name: "Instafill.ai",
         description: "AI PDF filler. Web application that helps user to fill PDF forms with AI.",
-        image: "/project_images/instafill.png",
+        image: "/images/last_projects/instafill.png",
         tags: ["PDF filler", "AI-driven", "Articles"],
         responsibilities: [
             "Support and develop new UI/UX features for web-app",
-            "Create multiple tools to work with pdf files"
+            "Create multiple tools to work with pdf files",
+            "Create admin tools to support clients and manage website"
         ],
         video: "https://www.youtube.com/channel/UCa57I5DFqulQaoMR_0H--kA",
         website: "https://instafill.ai",
         show: true
     },
     {
+        name: "Resume Copilot",
+        description: "AI-povered tool to create resumes.",
+        image: "/images/last_projects/resumecopilot.png",
+        tags: ["Resume builder", "AI-driven"],
+        responsibilities: [
+            "Support and develop new UI/UX features for web-app",
+            "Creating new resume templates for customers"
+        ],
+        video: "https://www.youtube.com/watch?v=KxM6UBC7yFw&t=1s",
+        website: "https://resumecopilot.net",
+        show: true
+    },
+    {
         name: "Best AI Apps",
         description: "Market of plugins for ChatGPT. AI applications designed for personal and business use cases.",
-        image: "/project_images/bestaiapps.png",
+        image: "/images/last_projects/bestaiapps.png",
         tags: ["AI plugins", "AI-driven"],
-        responsibilities: [],
+        responsibilities: [
+            "Support and develop new UI/UX features for web-app",
+            "Creating chat bots driven by AI to implement plugins features"
+        ],
         video: "https://www.youtube.com/@bestaiapps-co",
         website: "https://bestaiapps.co",
         show: true
@@ -36,7 +53,7 @@ export const projects: Project[] = [
     {
         name: "Tarta.ai",
         description: "Job board website. Also provides content about companies and articles for job apply tips.",
-        image: "/project_images/tarta.png",
+        image: "/images/last_projects/tarta.png",
         tags: ["Job board", "AI-driven", "Articles"],
         responsibilities: [
             "Support and develop new conted and functionality of website",
